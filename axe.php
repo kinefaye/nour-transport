39798997a6d65f76f8159a5d3ba57e03693a13ba
haoussakante <105302121+haoussakante@users.noreply.github.com>
2022-05-18 14:13:43 +0000

    Add files via upload

---
+++ axe.php
@@ -0,0 +1,115 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="axe1.css">
+    <title>Site Vitrine</title>
+</head>
+<body>
+<div class="entete4">    
+<img src="logo.jpg" alt="">  
+  <nav class="yave">
+        <ul>
+          <a href="index\index.php"><li>Accueil</li></a>
+         <a href="se.phrvice.php"><li>Service</li></a>
+           <a href="axe.php"> <li>Axe</li></a>
+           <a href="contact\cont.php"><li>Contact</li></a>
+           <a href="propos.php"><li>Apropos</li></a> 
+        </ul>
+  </nav>
+</div>
+   <div class="enete"> <div class="v">Notre compagnie <br> NOUR voyages /</div>  <div class="col">Nos voyages</div> 
+<div class="axes"> <div class="b">NOS AXES</div> </div>
+</div> 
+<img src="TOUTES-NOS-ESCALES.jpg" alt="">
+<div class="bamako"> <div class="gras">DE BAMAKO</div></div>
+<div class="bko"> <div class="gras">BAMAKO</div> </div>
+<div class="destination">
+<div class="jaune">Abidjan: 17 Heures/18 Heures      25 000 F CFA / 38 €  Allez</div>
+ <div class="bleu">Dakar:  16 Heures/17 Heures	   25 000 F CFA / 38 €  Allez</div>
+<div>Gao:     06 Heures/07 Heures	   22 000 F CFA / 34 €  Allez</div>
+<div class="bleu">Kayes:   16 Heures	/17 Heures  	9 000 F CFA / 14 €	Allez</div>
+<div class="jaune">Niamey:  05 Heures/06 Heures	    25 000 F CFA / 38 € Allez</div>
+<div class="bleu">Niono:   13 Heures/14 Heures	     4 000 F CFA / 6 €	Allez</div>
+<div class="noir">Nioro:   06 Heures/07 Heures    	 7 000 F CFA / 11 €	Allez</div>
+<div class="jaune">Nouakchott: 15 Heures/16 Heures    25 000 F CFA / 38 € Allez</div>
+<div class="bleu">Tombouctou:  	06 Heures/Jeudis et Samedis/07 Heures   
+                                        25 000 F CFA / 38€  Allez
+</div>
+</div>
+<div class="go"> <div class="gras"> VERS BAMAKO</div> </div>
+<div class="bk"> <div class="gras"> BAMAKO</div> </div>
+<div class="allant">
+    <div class="aun">Abidjan: 04 Heures/04 Heures      25 000 F CFA / 38 €  Allez</div>
+    <div class="ble">Dakar:  16 Heures/18 Heures	    25 000 F CFA / 38 €  Allez</div>
+    <div>Gao:                 04 Heures/05 Heures	    19 000 F CFA / 29 €  Allez</div>
+    <div class="ble">Kayes:   16 Heures	/17 Heures  	9 000 F CFA / 14 €	Allez</div>
+    <div class="aun">Niamey:  03 Heures30/04 Heures    25 000 F CFA / 38 € Allez</div>
+    <div class="ble">Niono:   13 Heures/14 Heures	     4 000 F CFA / 6 €	Allez</div>
+    <div class="noi">Nioro:   06 Heures/07 Heures    	 7 000 F CFA / 11 €	Allez</div>
+    <div class="aun">Nouakchott: 15 Heures/16 Heures    25 000 F CFA / 38 € Allez</div>
+    <div class="ble">Tombouctou:  	06 Heures/Jeudis et Samedis/07 Heures   
+                                                     25 000 F CFA / 38€  Allez
+   </div>   
+</div>
+<div class="ad"> <div class="gras">AUTRES DESTITIONS</div> </div>
+<div class="at"> <div class="gras"> AUTRES DESTITIONS</div> </div>
+
+<div class="autres">
+<div class="jo">Abidjan Niamey   03 Heures	/04 Heures   35 000F CFA/54 €     Allez</div>
+<div class="be">Niamey  Abidjan  03 Heures 30/	04 Heures  	35 000 F CFA / 54 €  Allez</div>
+<div>Gao    Niamey	  04 Heures 30/	05 Heures	8 500 F CFA / 13 €	 Allez</div>
+<div class="jo">Niamey  Gao	  03 Heures 30	/04 Heures	8 500 F CFA / 13 €   Allez</div>
+<div class="be">Abidjan                    	Ouaga</div>
+<div>Ouaga                        	Abidjan</div>
+<div class="jo">Niamey                         Ouaga</div>
+<div class="be">Ouaga                          Niamey</div>
+</div>
+</body>
+<footer>
+    <div class="moh">
+<div class="face"> <img src="facebook2.png" alt=""></div>
+<div class="twi"> <img src="twitter.png" alt="twit"></div>
+ <div class="pls"> <img src="plus.jpg" alt="plus"></div>
+ </div>
+   <div class="van"> 
+   <div class="pied"> <div class="a">PAIEMENTS</div>
+   <div class="fatou">
+<div class="im"><img src="visa.png" alt=""></div>
+<div class="ima"><img src="PAIEMENT-ORANGE-MONEY (1).png" alt=""></div>
+</div>
+<div class="sidi">
+<div class="imag"><img src="mastercard.png" alt=""></div>
+<div class="image"><img src="PayPal-V-300x116.png" alt=""></div>
+</div>
+<div class="bh">CGV & CONFIDENTIALITÉ<br> Conditions Générales <br> Confidentialité et Cookies <br> FAQ</div>
+<div class="c"> <img src="NOUR-TRANSPORT-VOYAGEURS-removebg-preview.png" alt=""></div>
+<div class="titre">DERNIÈRES ACTUS</div>
+<div class="d"><img src="Tombouctou-100x100.jpg" alt=""></div>
+<div class="da">
+    OUVERTURE D’UN NOUVEL <br>
+     AXE: BAMAKO-TOMBOUCTOU- <br>
+    BAMAKO</div>
+<div class="e"><img src="Mopti-150x150.jpg" alt=""></div>
+<div class="ea">OUVERTURE DE <br> L’AXE BAMAKO- <br>
+    MOPTI-BAMAKO</div>
+<div class="fff"><img src="Bamako-Kayes-150x150.jpg" alt=""></div>
+<div class="fa">DU NOUVEAU SUR <br>
+    L’AXE BAMAKO-KAYES- <br>
+   BAMAKO</div>
+   <div class="h1">NOTRE NEWSLETTER</div>
+   <div class="nom">Nom</div>
+   <input type="text" class="name" nom="name" required
+          minlength="4" maxlength="20" size="10">
+   <div class="email">Email</div>
+   <input type="text" class="imail" imail="imail" required
+          minlength="4" maxlength="8" size="10">
+   <div class="abon"> <div class="je">Je m’abonne</div></div>
+   </div> 
+   <div class="nour"> <div class="yu">© 2022 – NOUR TRANSPORT VOYAGEURS</div></div>
+</div>
+</footer>
+</html>
