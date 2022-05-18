90c264d96c350236e6590cefd58cba9d698746bc
haoussakante <105302121+haoussakante@users.noreply.github.com>
2022-05-18 14:09:03 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="accueil.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="entete2">    
+<img src="logo.jpg" alt="">  
+  <nav class="yave">
+        <ul>
+           <a href="../index\index.php"> <li>Accueil</li></a>
+            <a href="../service/seRvice.php"><li>Service</li></a>
+            <a href="../axe/axe.php"><li>Axe</li></a>
+            <a href="../contact/cont.php"><li>Contact</li></a>
+            <a href="../apropos/propos.php"><li>Apropos</li></a>
+        </ul>
+  </nav>
+</div>
+<div class="xamp"> <img src="IMG_2514 (2).JPG" alt="">  </div>
+
+    </div>
+    <div class="vic">NOS SERVICES</div>
+    <div class="mili">
+        <div class="tit">TRANSPORT DE PERSONNES</div>
+        <div class="n">Nous vous <br> proposons un <br> 
+            service de transport <br>
+            pour vos voyages <br>
+            d’affaires. Nos <br>
+            cars sont équipés <br>
+            de tous <br> équipements nécessaires <br>
+            pour vous assurer <br> 
+            un voyage sans fatigue.
+        </div>
+    <div class="ti">TRANSPORT DE BIENS</div>
+    <div class="nn">NOUR <br> TRANSPORT <br>
+         VOYAGEURS offre <br>
+         une gamme de <br>
+          transport de vos <br>
+         biens adaptés à <br>
+         vos besoins avec <br>
+         des véhicules dotés <br>
+         des meilleurs <br>
+          équipements <br>
+         de sécurité.</div>
+    <div class="til">TRANSPORT DE MARCHANDISES</div>
+    <div class="nnn">Nous <br> transportons vos <br>
+         Marchandises <br> dans toutes les <br>
+         villes de nos <br>
+         destinations. <br>
+         NTV est le service <br>
+         de transport unique <br>
+         qui vous garantie <br>
+          la sécurité.</div>
+    <div class="tim">LOCATION DE BUS </div>
+    <div class="nna">NTV est un <br> partenaire de <br>
+         confiance en <br> matière de location <br>
+         de bus et autocar. <br>
+         Nos véhicules sont <br>
+         dotés des <br> équipements <br>
+         de dernière <br> génération pour <br>
+         vous assurer <br> la tranquillité.</div>
+</div>
+<div class="max">
+<div class="dg"> <img src="DG-removebg-preview.png" alt="" width="100%"; height="50%">  </div>
+<div class="div">MOTS DE BIENVENUE DU DIRECTEUR GENERAL</div>
+<div class="gd">Cher(s) client(e)s et Internaute(s), <br>
+    Nous sommes très ravis de votre visite  sur notre portail <br>
+     web et nous espérons que vous y trouverez la réponse à <br>
+      vos questions et besoins en parcourant celui-ci. Nous <br>
+      investissons énormément dans les équipements de dernières <br>
+     générations et dans les technologies de l’information et <br>
+     de la communication dans le but de vous apporter assurance, <br>
+     confort et sécurité pour tous vos projets de voyages, de <br>
+     transport de marchandises, de location de bus et nous nous <br>
+     efforçons à être à la pointe dans le domaine des Bâtiments <br>
+     Travaux Publics et Particuliers,  de l’hydrocarbure en vous <br>
+     offrant un service exemplaire à travers nos stations services <br>
+     et également un service de commerce général pour tout vos besoins <br>
+     en divers tels que le thé ANTILOPE, la  <br>
+     vente de véhicule de tous genres…Bonne visite.
+</div>
+</div>
+</body>
+<footer>
+    <div class="moh">
+<div class="face"> <img src="facebook2.png" alt=""></div>
+<div class="twi"> <img src="twitter.png" alt="twit"></div>
+ <div class="pls"> <img src="plus.jpg" alt="plus"></div>
+ </div>
+   <div class="van"> 
+   <div class="pied"> <div class="a">PAIEMENTS</div>
+   <div class="fatou">
+<div class="im"><img src="visa.png" alt=""></div>
+<div class="ima"><img src="PAIEMENT-ORANGE-MONEY (1).png" alt=""></div>
+</div>
+<div class="sidi">
+<div class="imag"><img src="mastercard.png" alt=""></div>
+<div class="image"><img src="PayPal-V-300x116.png" alt=""></div>
+</div>
+<div class="b">CGV & CONFIDENTIALITÉ<br> Conditions Générales <br> Confidentialité et Cookies <br> FAQ</div>
+<div class="c"> <img src="NOUR-TRANSPORT-VOYAGEURS-removebg-preview.png" alt=""></div>
+<div class="titre">DERNIÈRES ACTUS</div>
+<div class="d"><img src="Tombouctou-100x100.jpg" alt=""></div>
+<div class="da">
+    OUVERTURE D’UN NOUVEL <br>
+     AXE: BAMAKO-TOMBOUCTOU- <br>
+    BAMAKO</div>
+<div class="e"><img src="Mopti-150x150.jpg" alt=""></div>
+<div class="ea">OUVERTURE DE <br> L’AXE BAMAKO- <br>
+    MOPTI-BAMAKO</div>
+<div class="fff"><img src="Bamako-Kayes-150x150.jpg" alt=""></div>
+<div class="fa">DU NOUVEAU SUR <br>
+    L’AXE BAMAKO-KAYES- <br>
+   BAMAKO</div>
+   <div class="h1">NOTRE NEWSLETTER</div>
+   <div class="nom">Nom</div>
+   <input type="text" class="name" nom="name" required
+          minlength="4" maxlength="20" size="10">
+   <div class="email">Email</div>
+   <input type="text" class="imail" imail="imail" required
+          minlength="4" maxlength="20" size="10">
+   <div class="abon"> <div class="je">Je m’abonne</div></div>
+   </div> 
+   <div class="nour"> <div class="yu">© 2022 – NOUR TRANSPORT VOYAGEURS</div></div>
+</div>
+</footer>
+</html>
