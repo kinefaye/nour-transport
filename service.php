0ea29b7d15eed32d6f5dc3e7b5617c8cfb0e511a
haoussakante <105302121+haoussakante@users.noreply.github.com>
2022-05-18 14:11:03 +0000

    Add files via upload

---
+++ service.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="services.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="entete1">    
+<img src="logo.jpg" alt="">  
+  <nav class="yave">
+        <ul>
+        <a href="../index/index.php"><li>Accueil</li></a>
+            <a href="../service/service.php"><li>Service</li></a>
+            <a href="../axe/axe.php"> <li>Axe</li></a>
+           <a href="../contact/cont.php"> <li>Contact</li></a>
+            <a href="../apropos/propos.php"> <li>Apropos</li></a>
+        </ul>
+  </nav>
+</div>
+  <div class="tit">
+      <div class="ba">NOUR BTP ET FORAGE</div>
+    </div> 
+
+   <div class="ip"> <img src="NOUR-HYDROCARBRE.jpg" alt="">  </div>
+   <div class="tex">NOUR BTP ET FORAGE est une des leaders entreprise <br>
+    au Mali réputée par la qualité de ses réalisations dans les <br>
+    domaines des Bâtiments, Travaux Publics et Particuliers et <br>
+    des Forages. Nous mettons à votre disposition notre expertise <br>
+    pour votre entière satisfaction. La filiale BTP et Forage de NOUR <br>
+    n'est pas seulement une entreprise, mais une véritable partenaire <br>
+    pour ses clients. </div>
+    <div class="imn"><img src="LES-BTP.jpg" alt=""> </div>
+    <div class="te">Bâtiments Civils, <br>
+        -Bâtiments Industriels, <br>
+        -Bâtiments Administratifs, <br>
+        -Bâtiments Commerciaux, <br>
+        -Infrastructure Urbaines, <br>
+        -Équipements publics, <br>
+        -Ouvrages Hydrauliques, <br>
+        -Ponts, <br>
+        -Chaussées</div>
+        <div class="imae"> <img src="LES-FORAGES.jpg" alt="">  </div>
+        <div class="tx">-Forage d'Eau <br>
+            -Forage Minier <br>
+            -Forage Géothermique <br>
+            -Forage Sonique <br>
+            -Forage Directionnel <br>
+            -Forage de Pieux</div>
+            <div class="tet">NOS MOYENS MATERIELS ET RÉALISATIONS</div>
+            <div class="ia"> <img src="téléchargement.png" alt="">  </div>
+            <div class="tie"> <div class="bj">NOUR HYDROCARBURE</div></div>
+            <div class="mi"> <img src="NOUR-HYDROCARBRE.jpg" alt="">  </div>
+            <div class="c">La filiale NOUR HYDROCARBURES est <br>
+                spécialisée dans l’importation, la distribution <br>
+               et la commercialisation des produits hydrocarbures <br>
+                tels que l’Essence, le Gaz-oil et le Gaz à travers ses <br> 
+               Stations Services implantées principalement dans la <br>
+                Région de Gao. Les produits des Stations NOUR sont <br>
+               certifiés ISO 9001:2015 apportant à vos engins une <br>
+                performance incomparable.</div>
+                <div class="dm">NOS PRODUITS</div>
+                <div class="o"><img src="LES-CARBURANTS-V.jpg" alt="">  </div>
+                <div class="e">-Super Sans Plomb <br>
+                    -Gaz-oil <br>
+                    -Pétrole lampant</div> 
+              <div class="lib"> <img src="LES-LUBRIFIANTS.jpg" alt=""> </div>
+             <div class="f">-Lubrifiant pour moteur diesel <br>
+                        -Lubrifiant pour moteur à essence <br>                       
+                        -Huile minérale</div>
+             <div class="gaz"> <img src="LES-GAZ.jpg" alt=""> </div>
+             <div class="g">-Gaz Butane 6 kg <br>
+                            -Gaz Butane 12,5 kg <br>                        
+                            -Gaz Butane 24 kg  <br>                  
+                            -Gaz Butane 32 kg  <br>
+              </div>
+    <div class="nos">NOS STATIONS SERVICES</div>
+    <div class="mg"> <img src="NTV-HYDROCARBURE.jpg" alt="">  </div>
+    <div class="mig"> <img src="téléchargement.jpg" alt="">  </div>
+    <div class="mag"> <img src="téléchargement (1).jpg" alt="" width="85%"; height="75%">  </div>
+    <div class="nou"> <div class="h"> NOUR COMMERCE GÉNÉRAL</div></div>
+    <div class="com"> <img src="NOUR-COMMERCE-GENERAL-W.jpg" alt="" width="70%"; height="90%">  </div>
+    <div class="co">NOUR COMMERCE GÉNÉRALE <br>
+        est spécialisée dans l'importation, <br>
+       l'exportation et la Vente générale <br>
+       d'articles dans le domaine de l'alimentation, <br>
+       les engins de toutes sortes et pleins d'autres <br>
+       produits. Elle est le distributeur du thé vert de <br>
+        chine de la marque ANTILOPE. </div>
+        <div class="mr"> <img src="NOUR-COMMERCE-V.jpg" alt="" width="85%"; height="90%">  </div>
+        <div class="mer">Nous commercialisons divers articles entre autre: <br>
+            -Thé vert ENTILOPE <br>
+            -Les Véhicules de tout genre <br>
+            -Les motos de tout genre <br>
+            -Les produits alimentaires <br>
+            -Les matériels de construction</div>
+            <div class="ph">NOS PRODUITS PHARES</div>
+            <div class="the"> <img src="the-noir-noel-a-vienne.jpg" alt="" width="70%"; height="100%"> </div>
+
+</body>
+<footer>
+    <div class="zap">
+    <div class="face"> <img src="facebook2.png" alt=""> </div>
+<div class="twi"> <img src="twitter.png" alt="twit"></div>
+ <div class="pls"> <img src="plus.jpg" alt="plus"></div>
+ </div>
+   <div class="van"> 
+   <div class="pied"> <div class="ssa">PAIEMENTS</div>
+   <div class="lol">
+<div class="im"><img src="visa.png" alt=""></div>
+<div class="ima"><img src="PAIEMENT-ORANGE-MONEY (1).png" alt=""></div>
+</div>
+<div class="mdr">
+<div class="imag"><img src="mastercard.png" alt=""></div>
+<div class="image"><img src="PayPal-V-300x116.png" alt=""></div>
+</div>
+<div class="b">CGV & CONFIDENTIALITÉ<br> Conditions Générales <br> Confidentialité et Cookies <br> FAQ</div>
+<div class="cs"> <img src="NOUR-TRANSPORT-VOYAGEURS-removebg-preview.png" alt=""></div>
+<div class="titre">DERNIÈRES ACTUS</div>
+<div class="d"><img src="Tombouctou-100x100.jpg" alt=""></div>
+<div class="da">
+    OUVERTURE D’UN NOUVEL <br>
+     AXE: BAMAKO-TOMBOUCTOU- <br>
+    BAMAKO</div>
+<div class="et"><img src="Mopti-150x150.jpg" alt=""></div>
+<div class="ea">OUVERTURE DE <br> L’AXE BAMAKO- <br>
+    MOPTI-BAMAKO</div>
+<div class="fff"><img src="Bamako-Kayes-150x150.jpg" alt=""></div>
+<div class="fa">DU NOUVEAU SUR <br>
+    L’AXE BAMAKO-KAYES- <br>
+   BAMAKO</div>
+   <div class="h1">NOTRE NEWSLETTER</div>
+   <div class="nom">Nom</div>
+   <input type="text" class="name" nom="name" required
+          minlength="4" maxlength="8" size="10">
+   <div class="email">Email</div>
+   <input type="text" class="imail" imail="imail" required
+          minlength="4" maxlength="20" size="10">
+   <div class="abon"> <div class="je">Je m’abonne</div></div>
+   </div> 
+   <div class="nour"> <div class="yu">© 2022 – NOUR TRANSPORT VOYAGEURS</div></div>
+</footer>
+</html>
