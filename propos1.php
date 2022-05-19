6209050132a5be7d0bc4097241a204aba21253f1
haoussakante <105302121+haoussakante@users.noreply.github.com>
2022-05-19 16:14:53 +0000

    Add files via upload

---
+++ propos1.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="image1"> <img src="NOUR-TRANSPORT-VOYAGEURS.jpg" alt=""></div>
+  
+     <a href="../index.php">  <div class="ac">Accueil</div></a>
+     <a href="../service/service.php"> <div class="ser">Service</div></a>
+     <a href="../axe/axe.php"> <div class="axe">Axe</div></li> </a>
+     <a href="../contact/cont.php">  <div class="con">Contact</div> </a>
+     <a href="../apropos/propos1.php">  <div class="me">Apropos</div> </a>
+  <br>
+  <div id="text">2014 - 2022 : Bientôt 18 ans de passion au service du transport et du voyage !</div><br>
+  <div id="texte">L'ADMINISTRATION</div><br>
+  <div class="admini">
+    <div class="dg"><img src="Directrice.jpg" alt="" width="300px" height="300px">
+      <div class="indg"> Directrice Generale</div>
+    </div>
+    <div class="df"><img src="directeur-financier.webp" alt=""  width="300px" height="300px">
+       <div class="indf"> Directeur financier</div>
+    </div>
+    <div class="sc"><img src="SECRETAIRE.jfif" alt=""  width="300px" height="300px">
+       <div class="insc"> Secretaire</div>
+    </div>
+  </div><br>
+     <br>
+     <div class="eq">
+     <div class="equi"></div>
+    <img src="equipe.jpg" alt=""  width="600px" height="300px"></div>
+    <div class="reseq">Une Equipe Dynamique</div>
+    <br>
+  
+    <div class="societe">NOTRE SOCIETE</div>
+    <div class="conteneur">
+  <div class="contenu" >
+  <p>
+Comme son nom l’indique, NOUR « Lumière » en français, la société NOUR TRANSPORT VOYAGEURS 
+SARL a été créée en 2014 dans le but d’éclairer, de faciliter, d’apporter un changement 
+dynamique et positif dans les politiques de développement du secteur privé au Mali 
+et dans la sous-région Ouest Africaine. Elle est une société de droit commun qui a 
+pour vocation : le transport des personnes, des biens et services, les locations et 
+les ventes de véhicules de tous genres. Et à cette activité s’ajoute celles du 
+Commerce Général (Commerce de thé et autres marchandises) et les Bâtiments Travaux 
+Publics et Particuliers et les Forages. Disposant de vingt-six (26) autobus comme 
+parc automobile reparti entre deux axes : Bamako-Dakar, Bamako-Niamey en passant par
+Gao. Pour mener à bien ses activités, la société s’est doté d’un personnel administratif
+(chef d’escales, comptables, contrôleurs, guichetier, chargeur) , et d’un personnel
+mécanique (chauffeurs, mécaniciens, apprentis).Pour le moment elle dispose 
+de 19 escales (3 à Bamako, Ségou, San, Sévaré, Douentza, Hombori, Boni, Gossi,
+Gao, Ansongo, Ayerou, Niamey, Diéma, Kayes, Diboli, Kaolack, Dakar) et envisage 
+dans l’avenir des nouvelles escales à l’intérieur du pays (Koutiala,…) ainsi que dans 
+les sous-régions (Lomé, Ghana ,…) en fonction de l’agrandissement de son parc automobile.
+a élargi non seulement son parc automobile avec l’achat des nouveaux bus MCV et aussi de 
+nouvelles destinations telles que la Côte d’ivoire (Abidjan, Yamoussoukro, Bouaké, Ouangolo, 
+Pogo…), le Burkina Faso (Ouagadougou, Bobo Dioulasso,…), la Mauritanie (Nouakchott, Gogui, 
+Koubeni, Kiffa,…) et le Niger en passant par le Burkina et en passant par Abidjan et aussi 
+plusieurs villes à l’intérieur du Mali telle que Nioro du Sahel, Niono, Sikasso, Koutiala, 
+Bla etc… Pour plus de confort et dans le souci de fidéliser toujours ses clients, 
+la société NOUR a ouvert certains points de vente de ticket à Bamako à savoir Badialan, 
+Médine et N’golonina afin de se rapprocher de ses clients et aussi des services d’achat de 
+billet à distance.
+    </p></div>
+    <div class="car">
+    <div class="image3"> <img src="IMAGE CAR 2.jpg" alt=""></div>
+    <div class="image4"> <img src="Image car 1.jpg" alt=""></div>
+  </div>
+  </div>
+
+<div class="bloc">
+  <div class="image5"><img src="facebook2.png" alt=""></div>
+  <div class="image6"><img src="twitter.png" alt=""></div>
+  <div class="image7"><img src="plus.jpg" alt=""></div>
+  </div>
+    
+   <div class="pied"> <div class="a">PAIEMENTS</div>
+<div class="image8"><img src="visa.png" alt=""></div>
+<div class="image9"><img src="PAIEMENT-ORANGE-MONEY (1).png" alt=""></div>
+<div class="image10"><img src="mastercard.png" alt=""></div>
+<div class="image11"> <img src="PayPal-V-300x116.png" alt=""></div>
+<div class="b">CGV & CONFIDENTIALITÉ<br> Conditions Générales <br> Confidentialité et Cookies <br> FAQ</div>
+<div class="image12"> <img src="NOUR-TRANSPORT-VOYAGEURS-removebg-preview.png" alt=""></div>
+<div class="titre">DERNIÈRES ACTUS</div>
+<div class="image13"><img src="Tombouctou-100x100.jpg" alt=""></div>
+<div class="da">
+    OUVERTURE D’UN NOUVEL <br>
+     AXE: BAMAKO-TOMBOUCTOU- <br>
+    BAMAKO</div>
+<div class="image14"><img src="Mopti-150x150.jpg" alt=""></div>
+<div class="ea">OUVERTURE DE <br> L’AXE BAMAKO- <br>
+    MOPTI-BAMAKO</div>
+<div class="image15"><img src="Bamako-Kayes-150x150.jpg" alt=""></div>
+<div class="fa">DU NOUVEAU SUR <br>
+    L’AXE BAMAKO-KAYES- <br>
+   BAMAKO</div>
+   <div class="h1">NOTRE NEWSLETTER</div>
+   <div class="nom">Nom</div>
+   <input type="text" class="name" nom="name" required
+          minlength="4" maxlength="20" size="10">
+   <div class="email">Email</div>
+   <input type="text" class="imail" imail="imail" required
+          minlength="10" maxlength="50" size="10">
+   <div class="abon"> <div class="je"><button type="submit">Je m’abonne</div></div>
+   </div> 
+   <div class="nour"> 
+     <div class="yu">© 2022 – NOUR TRANSPORT VOYAGEURS</div></div>
+</body>
+</html>
